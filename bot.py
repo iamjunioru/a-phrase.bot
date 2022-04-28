af0d857f44b4cc36a77143debfebc3369958a0a5
Junior <iamjunioru@gmail.com>
2022-04-28 16:31:10 -0300

    api connect + tokens

--- bot.py
+++ bot.py
@@ -5,3 +5,14 @@ from time import sleep
 from httplib2 import Response
 import tweepy
 import random
+
+# twitter developers api connect
+bearer_token = ''
+api_key = ''
+api_key_secret = ''
+access_token = ''
+access_token_secret = ''
+
+# connect
+auth = tweepy.OAuthHandler(api_key, api_key_secret, access_token=access_token, access_token_secret=access_token_secret, callback=Response)
+api = tweepy.API(auth)
